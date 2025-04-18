6169a3bd2786ce7905a128ced5ad9dbab0bb04b9
Kumar Satyam <80621368+imdsatyam@users.noreply.github.com>
2025-04-18 16:09:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,14 @@ import Banner from "./Components/Banner";
 import Description from "./Components/Description";
 import Footer from "./Components/Footer";
 import SuccessStories from "./Components/SuccessStories";
-import DonateNow from "./Components/DonateNow"
-import OurLeader from "./Components/OurLeader"
-import RecentNews from "./Components/RecentNews"
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navigation />
-
-        <Routes>
-          <Route exact path="*" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+import DonateNow from "./Components/DonateNow";
+import OurLeader from "./Components/OurLeader";
+import RecentNews from "./Components/RecentNews";
+import OurProgram from "./Components/OurProgram";
+import Impact from "./Components/Impact";
+import Resources from "./Components/Resources";
+import AboutUs from "./Components/AboutUs";
+import Contact from "./Components/Contact";
 
 function Home() {
   return (
@@ -34,6 +25,24 @@ function Home() {
       <RecentNews />
       <Footer />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/our-program" element={<OurProgram />} />
+        <Route path="/impact" element={<Impact />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/Description-us" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/donate" element={<DonateNow />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
