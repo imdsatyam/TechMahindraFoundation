5779013291325e1d2f823484cfda6943f5edb62c
Kumar Satyam <80621368+imdsatyam@users.noreply.github.com>
2025-04-18 16:09:28 +0530

    Update Navigation.js

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -12,33 +12,19 @@ function Navigation() {
   return (
     <nav className="mainNav">
       <div className="logo">
-        <a href="#Home" className="navLogo">
+        <Link to="/" className="navLogo">
           <img src={Logo} alt="Logo" />
-        </a>
+        </Link>
       </div>
       <div className="homeNav">
         <ul className={`navList ${menuOpen ? 'mobile-active' : ''}`}>
-          <li className="nav__item">
-            <Link to="/" className="navLink">Home</Link>
-          </li>
-          <li className="nav__item">
-            <Link to="/our-program" className="navLink">Our Program</Link>
-          </li>
-          <li className="nav__item">
-            <Link to="/impact" className="navLink">Impact</Link>
-          </li>
-          <li className="nav__item">
-            <Link to="/resources" className="navLink">Resources</Link>
-          </li>
-          <li className="nav__item">
-            <Link to="/Description-us" className="navLink">Description Us</Link>
-          </li>
-          <li className="nav__item">
-            <Link to="/contact" className="navLink">Contact</Link>
-          </li>
-          <li className="nav__item">
-            <Link to="/donate" className="navLink">Donate</Link>
-          </li>
+          <li><Link to="/" className="navLink">Home</Link></li>
+          <li><Link to="/our-program" className="navLink">Our Program</Link></li>
+          <li><Link to="/impact" className="navLink">Impact</Link></li>
+          <li><Link to="/resources" className="navLink">Resources</Link></li>
+          <li><Link to="/Description-us" className="navLink">Description Us</Link></li>
+          <li><Link to="/contact" className="navLink">Contact</Link></li>
+          <li><Link to="/donate" className="navLink">Donate</Link></li>
         </ul>
         <div className="hamburger-menu" onClick={toggleMenu}>
           <div></div>
